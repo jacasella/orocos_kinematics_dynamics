41de836ced519bfcd6dd620f10cd0ae0e028e195
Dennis Chavez Romero <dennis.chavezromero.com>
2021-12-10 18:04:26 -0500

    Created build folder to build orocos_kdl library

---
+++ orocos_kdl/build/examples/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/Beachbots/Code/catkin_ws/src/orocos_kinematics_dynamics/orocos_kdl/examples
+# Build directory: /home/pi/Beachbots/Code/catkin_ws/src/orocos_kinematics_dynamics/orocos_kdl/build/examples
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ orocos_kdl/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/pi/Beachbots/Code/catkin_ws/src/orocos_kinematics_dynamics/orocos_kdl/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
